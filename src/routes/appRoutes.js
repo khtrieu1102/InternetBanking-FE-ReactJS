f4f15c868595f3ea96252607c5f046ae7309dcb3
KhacTrieu <khtrieu1102@gmail.com>
2020-06-13 15:09:12 +0700

    Add new screens, config adminRoute and customerRoute

--- src/routes/appRoutes.js
+++ src/routes/appRoutes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Redirect, Route } from "react-router-dom";
 
 import LoginConnect from "../BankingUI/Components/Authentication/Login/LoginConnect";
+import ForgotPasswordConnect from "../BankingUI/Components/Authentication/ForgotPassword/ForgotPasswordConnect";
 import NotFound from "../BankingUI/Components/Others/NotFound/NotFound";
 import Register from "../BankingUI/Components/Authentication/Register/Register";
 
@@ -9,8 +10,31 @@ import DefaultLayout from "../BankingUI/Components/Layout/DefaultLayout";
 
 import DashboardConnect from "../BankingUI/Components/Customer/Dashboard/DashboardConnect";
 import ReceiverList from "../BankingUI/Components/Customer/ReceiverList/ReceiverList";
+import FormEditInfoConnect from "../BankingUI/Components/Customer/FormEditInfo/FormEditInfoConnect";
+import TransactionConnect from "../BankingUI/Components/Customer/Transaction/TransactionConnect";
 
-export const PrivateRoute = ({ comp: Component, ...rest }) => {
+export const CustomerRoute = ({ comp: Component, ...rest }) => {
+	return (
+		<Route
+			{...rest}
+			render={(props) =>
+				rest.isAuthenticated ? (
+					<Component {...props} />
+				) : (
+					// children
+					<Redirect
+						to={{
+							pathname: "/login",
+							state: { from: props.location },
+						}}
+					/>
+				)
+			}
+		/>
+	);
+};
+
+export const AdminRoute = ({ comp: Component, ...rest }) => {
 	return (
 		<Route
 			{...rest}
@@ -37,30 +61,56 @@ export const public_routes = [
 		routetype: Route,
 		component: LoginConnect,
 	},
+	{
+		path: "/reset-password",
+		routetype: Route,
+		component: ForgotPasswordConnect,
+	},
 ];
 
-export const private_routes = [
+export const customer_routes = [
 	{
 		path: "/",
-		routetype: PrivateRoute,
+		routetype: CustomerRoute,
 		component: DashboardConnect,
 		layout: DefaultLayout,
 	},
 	{
 		path: "/edit",
-		routetype: PrivateRoute,
-		component: Register,
+		routetype: CustomerRoute,
+		component: FormEditInfoConnect,
 		layout: DefaultLayout,
 	},
 	{
 		path: "/receivers",
-		routetype: PrivateRoute,
+		routetype: CustomerRoute,
 		component: ReceiverList,
 		layout: DefaultLayout,
 	},
 	{
+		path: "/transaction",
+		routetype: CustomerRoute,
+		component: TransactionConnect,
+		layout: DefaultLayout,
+	},
+	{
 		path: "/*",
-		routetype: PrivateRoute,
+		routetype: CustomerRoute,
+		component: NotFound,
+		layout: DefaultLayout,
+	},
+];
+
+export const admin_routes = [
+	{
+		path: "/",
+		routetype: AdminRoute,
+		component: DashboardConnect,
+		layout: DefaultLayout,
+	},
+	{
+		path: "/*",
+		routetype: AdminRoute,
 		component: NotFound,
 		layout: DefaultLayout,
 	},
