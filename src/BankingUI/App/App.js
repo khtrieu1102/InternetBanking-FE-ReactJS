c064829bb5fddbbdbf0755b93df89db14c52160d
KhacTrieu <khtrieu1102@gmail.com>
2020-07-30 15:57:46 +0700

    Add an apiURL variable.

--- src/BankingUI/App/App.js
+++ src/BankingUI/App/App.js
@@ -43,13 +43,13 @@ const App = (props) => {
 	let notificationsData = [];
 
 	// --- CONFIG AXIOS ---
-	axios.defaults.baseURL = "http://localhost:5000";
+	const apiURL = process.env.REACT_APP_BASE_URL || "http://localhost:5000";
+	axios.defaults.baseURL = apiURL;
 	axios.defaults.headers.common[
 		"Authorization"
 	] = `Bearer ${authentication.accessToken}`;
 	axios.defaults.timeout = 15000;
-	axiosInstance.defaults.baseURL =
-		process.env.REACT_APP_BASE_URL || "http://localhost:5000";
+	axiosInstance.defaults.baseURL = apiURL;
 	axiosInstance.defaults.headers.common[
 		"Authorization"
 	] = `Bearer ${authentication.accessToken}`;
@@ -67,10 +67,6 @@ const App = (props) => {
 			)
 				return err.response.data;
 
-			if (err.response.status !== 401) {
-				return err.response.data;
-			}
-
 			return new Promise((resolve, reject) => {
 				const originalReq = err.config;
 
@@ -82,7 +78,7 @@ const App = (props) => {
 				) {
 					originalReq._retry = true;
 
-					let res = fetch("http://localhost:5000/api/auth/refresh", {
+					let res = fetch(`${apiURL}/api/auth/refresh`, {
 						method: "POST",
 						mode: "cors",
 						cache: "no-cache",
