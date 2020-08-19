31f06e7666856e5701dc10bf9c23a399756e504f
KhacTrieu <khtrieu1102@gmail.com>
2020-08-20 01:14:30 +0700

    Filter data by date + name bank

--- src/BankingUI/Components/Admin/TransactionStatistics/TransactionStatistics.js
+++ src/BankingUI/Components/Admin/TransactionStatistics/TransactionStatistics.js
@@ -1,14 +1,25 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Col, Row, Card, Container, Form } from "react-bootstrap";
-import { Table, Badge, Button } from "antd";
+import { Table, Badge, Button, Space, DatePicker } from "antd";
 import axios from "axios";
 import getBankName from "../../HelperFunctions/getBankName";
 import formatter from "../../HelperFunctions/moneyFormatter";
+import TransactionDetail from "../../Customer/TransactionManagement/TransactionDetail/TransactionDetail";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faBackward } from "@fortawesome/free-solid-svg-icons";
+
 const { Column, ColumnGroup } = Table;
+const { RangePicker } = DatePicker;
 
 const TransactionStatistics = (props) => {
 	const [transactionsData, setTransactionsData] = useState([]);
-	const [step, setStep] = useState(0);
+	const [filteredTransactionsData, setFilteredTransactionsData] = useState([]);
+	const [step, setStep] = useState("table");
+	const [workingTransaction, setWorkingTransaction] = useState({
+		sentUserId: "",
+	});
+	// const [fromDate, setFromDate] = useState(null);
+	let range = null;
 	const mountedRef = useRef(true);
 
 	useEffect(() => {
@@ -28,10 +39,113 @@ const TransactionStatistics = (props) => {
 			.then((result) => {
 				console.log(result);
 				setTransactionsData(result);
+				setFilteredTransactionsData(result);
 			})
 			.catch((error) => {
 				console.log(error.response);
 			});
+	};
+
+	const onFilter = (range) => {
+		const fromDateUTC = new Date(range.fromDate);
+		const toDateUTC = new Date(range.toDate);
+		const newTransactionData = transactionsData.filter((item) => {
+			const createdAtDate = new Date(item.createdAt);
+			if (fromDateUTC <= createdAtDate && toDateUTC >= createdAtDate)
+				return true;
+			return false;
+		});
+		console.log(newTransactionData);
+		setFilteredTransactionsData(newTransactionData);
+	};
+
+	const StatisticTable = () => {
+		return (
+			<>
+				<Space>
+					<RangePicker
+						onChange={(dates, dateStrings) => {
+							console.log("hello: ", dates, dateStrings);
+							range = { fromDate: dateStrings[0], toDate: dateStrings[1] };
+						}}
+					/>
+					<Button type="primary" onClick={() => onFilter(range)}>
+						Filter
+					</Button>
+				</Space>
+				<Table dataSource={filteredTransactionsData}>
+					<Column
+						title="Sent bank"
+						dataIndex="sentBankId"
+						key="sentBankId"
+						render={(sentBankId) => <>{getBankName(sentBankId)}</>}
+						filters={[
+							{ text: "SAPHASANBank", value: 0 },
+							{ text: "3TBank", value: 1 },
+							{ text: "BAOSON Bank", value: 2 },
+						]}
+						onFilter={(value, record) => record.sentBankId === value}
+					/>
+					<Column
+						title="Received bank"
+						dataIndex="receivedBankId"
+						key="receivedBankId"
+						render={(receivedBankId) => <>{getBankName(receivedBankId)}</>}
+						filters={[
+							{ text: "SAPHASANBank", value: 0 },
+							{ text: "3TBank", value: 1 },
+							{ text: "BAOSON Bank", value: 2 },
+						]}
+						onFilter={(value, record) => record.receivedBankId === value}
+					/>
+					<Column
+						title="Amount"
+						key="amount"
+						render={(row) => {
+							let moneyDetail, badgeColor;
+							if (row.receivedBankId === 0) {
+								moneyDetail = `+ ${formatter.format(row.amount)}`;
+								badgeColor = "#52c41a";
+							} else {
+								moneyDetail = `- ${formatter.format(row.amount)}`;
+								badgeColor = "red";
+							}
+							return (
+								<Badge
+									className="site-badge-count-109"
+									count={moneyDetail}
+									style={{ backgroundColor: badgeColor }}
+								/>
+							);
+						}}
+					/>
+					<Column
+						title="Date"
+						key="createdAt"
+						render={(row) => {
+							return <>{new Date(row.createdAt).toUTCString()}</>;
+						}}
+					/>
+					<Column
+						title="Action"
+						key="action"
+						render={(item) => (
+							<Button
+								type="primary"
+								onClick={() => {
+									setWorkingTransaction(
+										Object.assign(workingTransaction, item)
+									);
+									setStep("transaction-detail");
+								}}
+							>
+								Detail
+							</Button>
+						)}
+					/>
+				</Table>
+			</>
+		);
 	};
 
 	return (
@@ -41,62 +155,21 @@ const TransactionStatistics = (props) => {
 					<Card className="mt-3">
 						<Card.Header className="toolBar">TRA SOÁT GIAO DỊCH</Card.Header>
 						<Card.Body>
-							<Table dataSource={transactionsData}>
-								<Column
-									title="Sent bank"
-									dataIndex="sentBankId"
-									key="sentBankId"
-									render={(sentBankId) => <>{getBankName(sentBankId)}</>}
-								/>
-								<Column
-									title="Received bank"
-									dataIndex="receivedBankId"
-									key="receivedBankId"
-									render={(receivedBankId) => (
-										<>{getBankName(receivedBankId)}</>
-									)}
-								/>
-								<Column
-									title="Amount"
-									key="amount"
-									render={(row) => {
-										let moneyDetail, badgeColor;
-										if (row.receivedBankId === 0) {
-											moneyDetail = `+ ${formatter.format(row.amount)}`;
-											badgeColor = "#52c41a";
-										} else {
-											moneyDetail = `- ${formatter.format(row.amount)}`;
-											badgeColor = "red";
-										}
-										return (
-											<Badge
-												className="site-badge-count-109"
-												count={moneyDetail}
-												style={{ backgroundColor: badgeColor }}
-											/>
-										);
-									}}
-								/>
-								<Column
-									title="Address"
-									dataIndex="receivedUserId"
-									key="receivedUserId"
-								/>
-								<Column
-									title="Action"
-									key="action"
-									render={(item) => (
-										<Button
-											type="primary"
-											onClick={() => {
-												console.log(item);
-											}}
-										>
-											Detail
-										</Button>
-									)}
-								/>
-							</Table>
+							{step === "table" && <StatisticTable />}
+							{step === "transaction-detail" && (
+								<>
+									<Button
+										variant="light"
+										size="sm"
+										onClick={() => {
+											setStep("table");
+										}}
+									>
+										<FontAwesomeIcon icon={faBackward} /> BACK
+									</Button>
+									<TransactionDetail transactionDetail={workingTransaction} />
+								</>
+							)}
 						</Card.Body>
 					</Card>
 				</Col>
